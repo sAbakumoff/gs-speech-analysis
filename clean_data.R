1be2d1b543c857d917e1dc86beca4d9214e9f7a0
Sergey Abakumoff <s.abakumoff@gmail.com>
2016-10-17 21:58:19 +0700

    finished the basic script to extract the data

--- clean_data.R
+++ clean_data.R
@@ -1,10 +1,20 @@
+# install.packages("readr")
+
 library("readr")
 library("stringr")
 
 raw_content<-read_file("06042013 GS 1.txt")
 markers<-str_locate_all(raw_content, pattern="MS. CLINTON:|MR. BLANKFEIN:|PARTICIPANT:")
 markers<-markers[[1]]
-for(i in 1 : 3) {
-  message(substr(raw_content, markers[i,1], markers[i,2]))
-  message(str_trim(str_replace_all(substr(raw_content, markers[i,2] + 1, markers[i+1,1] - 1), "\r\n", "")))
+len<-length(markers[,1])
+speaker<-character(len)
+msg<-character(len)
+order<-numeric(len)
+speech_id<-rep("GS06042013", len)
+for(i in 1 : len) {
+  order[i] = i
+  speaker[i] = str_trim(str_replace_all(substr(raw_content, markers[i,1], markers[i,2] - 1), "MS.|MR.", ""))
+  msg_stop<-if(i==len) str_length(raw_content) else markers[i+1,1] - 1
+  msg[i] = str_trim(str_replace_all(substr(raw_content, markers[i,2] + 1, msg_stop), "\r\n|\\s+", " "))
 }
+df<-data.frame(speech_id, order, speaker, msg)
